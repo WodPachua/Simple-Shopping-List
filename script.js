ba86ceecc22c62115c69438e697c713e16c7c310
wodpachua <molarag21996@gmail.com>
2024-02-28 22:11:16 +0300

    Add function to add and delete items in list

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 const ulList = document.querySelector('ul');
 const inputBox = document.querySelector('input');
-const addButton = document.querySelector('button');
+const addItemButton = document.querySelector('button');
+
+addItemButton.addEventListener('click', addItem);
+
+function addItem() {
+    const item = inputBox.value;
+    inputBox.value = '';
+
+    const createItem = document.createElement('li');
+    const createSpan = document.createElement('span');
+    const createButton = document.createElement('button');
+
+    createItem.appendChild(createSpan);
+    createSpan.textContent = item;
+    createSpan.style.fontWeight = 'bolder';
+    createSpan.style.color = 'green';
+    createItem.appendChild(createButton);
+    createButton.textContent = 'Delete Item';
+    createButton.style.fontWeight = 'bolder';
+    createButton.style.color = 'white';
+
+    ulList.appendChild(createItem);
+
+    createButton.addEventListener('click', removeList)
+
+    function removeList() {
+        ulList.removeChild(createItem);
+    };
+
+    inputBox.focus();
+};
 
